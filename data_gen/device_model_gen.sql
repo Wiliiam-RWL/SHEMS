e9c36195abad7e05bb2a585e751834ecfd38160c
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-11-30 15:07:53 -0500

    generate device_model table

---
+++ data_gen/device_model_gen.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+    device_model (model_type, model_name)
+VALUES
+    (
+        'Refrigerator',
+        'Samsung Family Hub Refrigerator'
+    ),
+    (
+        'Refrigerator',
+        'LG Smart Wi-Fi Enabled Refrigerator'
+    ),
+    ('Air Conditioner', 'Daikin Smart Inverter AC'),
+    (
+        'Air Conditioner',
+        'Samsung Wind-Free Air Conditioner'
+    ),
+    (
+        'Light Bulb',
+        'Philips Hue White and Color Ambiance'
+    ),
+    ('Light Bulb', 'LIFX A60 Smart Bulb'),
+    ('Smart Speaker', 'Amazon Echo Dot'),
+    ('Smart Speaker', 'Google Nest Audio');
