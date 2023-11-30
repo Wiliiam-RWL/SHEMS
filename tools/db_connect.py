3aa02eddac50c9b4bc7c1be12b0579317dd67da3
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-11-30 11:49:19 -0500

    db connections

---
+++ tools/db_connect.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+try:
+    conn = mysql.connector.connect(
+        host="db-project.clt5dhnemxsz.us-east-2.rds.amazonaws.com",  # No 'https://' prefix
+        user="admin",
+        password="12345678",
+        database="shems",
+        port=3306,  # or your custom port
+    )
+    if conn.is_connected():
+        print("Connected to MySQL database")
+
+    # Database operations go here
+
+except mysql.connector.Error as e:
+    print(f"Error connecting to MySQL: {e}")
+finally:
+    if conn.is_connected():
+        conn.close()
+        print("Connection closed.")
