9eeaf6826198258e325ab30059ce87bf3b8344f5
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-11-30 15:08:37 -0500

    modifiy zip code

---
+++ data_gen/location_gen.sql
@@ -0,0 +1,98 @@
+INSERT INTO
+    location (
+        customer_id,
+        location_street_num,
+        location_street_name,
+        location_unit_number,
+        location_city,
+        location_state,
+        location_zipcode,
+        square_feet,
+        num_bedrooms,
+        num_occupants
+    )
+VALUES
+    (
+        1,
+        123,
+        '5th Avenue',
+        'Apt 1005',
+        'New York',
+        'NY',
+        '10003',
+        865.0,
+        2,
+        2
+    ),
+    (
+        1,
+        465,
+        'Washington Boulevard',
+        'Apt 1303',
+        'Jersey City',
+        'NJ',
+        '07310',
+        789.0,
+        2,
+        2
+    ),
+    (
+        2,
+        456,
+        'Madison Avenue',
+        'Apt 2406',
+        'New York',
+        'NY',
+        '10022',
+        713.0,
+        1,
+        2
+    ),
+    (
+        3,
+        789,
+        'Park Avenue',
+        'Apt 0701',
+        'New York',
+        'NY',
+        '11206',
+        803.0,
+        2,
+        3
+    ),
+    (
+        3,
+        25,
+        'Christopher Columbus Drive',
+        'Apt 3403',
+        'Jersey City',
+        'NJ',
+        '07302',
+        875.0,
+        2,
+        3
+    ),
+    (
+        4,
+        101,
+        'Broadway',
+        'Apt 2803',
+        'New York',
+        'NY',
+        '11249',
+        698.0,
+        2,
+        1
+    ),
+    (
+        5,
+        202,
+        'Wall Street',
+        'Apt 1502s',
+        'New York',
+        'NY',
+        '10005',
+        715.0,
+        1,
+        1
+    );
