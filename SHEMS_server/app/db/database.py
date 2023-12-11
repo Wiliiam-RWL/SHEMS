640216f605a5a35a4420d61717ea3f53ce020846
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-12-10 22:31:08 -0500

    added transaction

--- SHEMS_server/app/db/database.py
+++ SHEMS_server/app/db/database.py
@@ -2,12 +2,34 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from config import Config
 
+
 class Database:
     # Creating an SQLAlchemy engine with connection pool settings
     engine = create_engine(Config.DATABASE_URI, pool_size=10, max_overflow=20)
 
     # Using scoped_session to ensure thread safety
     db_session = scoped_session(sessionmaker(bind=engine))
+
+    @staticmethod
+    def begin_transaction():
+        """
+        Begins a new transaction.
+        """
+        return Database.db_session.begin_nested()
+
+    @staticmethod
+    def commit_transaction():
+        """
+        Commits the current transaction.
+        """
+        Database.db_session.commit()
+
+    @staticmethod
+    def rollback_transaction():
+        """
+        Rolls back the current transaction.
+        """
+        Database.db_session.rollback()
 
     @staticmethod
     def execute_query(query, params=None):
@@ -21,8 +43,22 @@ class Database:
         if params is None:
             params = {}
         result = Database.db_session.execute(query, params)
-        Database.db_session.commit()
         return result
+    
+    @staticmethod
+    def handle_transaction(queries_params: list(dict)):
+        """
+        Execute a series of queries as transaction
+
+        If successful, commit
+        If failed, rollback
+
+        :param queries_params: {'query': the query to be execute, 'params': the parameters}
+        """
+        try:
+            transaction = Database.begin_transaction()
+            for qp in queries_params:
+                Database.execute_query(qp['query'], qp['params'])
 
     @staticmethod
     def init_db():
